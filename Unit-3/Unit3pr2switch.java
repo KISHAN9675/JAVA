817fb39fc3c011e618d15d48e04d9f69ec6eb0c4
KISHAN9675 <sainikia9@gmail.com>
2025-07-18 08:19:55 +0530

    Switch case learn

---
+++ Unit-3/Unit3pr2switch.java
@@ -0,0 +1,40 @@
+import java.lang.runtime.SwitchBootstraps;
+import java.util.Scanner;
+
+public class Unit3pr2switch{ 
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+            int weekday;
+            System.out.print("enter the number " );
+            weekday = sc.nextInt();
+            switch (weekday) {
+                case 1:
+                    System.out.println("Monday");
+                    break;
+                case 2:
+                System.out.println("Tuesday");
+                    break;
+                case 3:
+                    System.out.println("Wednesday");
+                    break;
+                case 4:
+                System.out.println("Thursday");
+                    break;
+                     case 5:
+                System.out.println("Friday");
+                    break;
+                case 6:
+                System.out.println("Saturday");
+                    break;
+                 case 7:
+                System.out.println("Sunday");
+                    break;
+                
+                default:
+                System.out.println("wrong number");
+                    break;
+            }
+              
+        
+    }
+}
